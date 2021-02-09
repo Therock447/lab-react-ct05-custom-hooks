00626b784a848798f4e0fd57ee9bbb5648b40753
Franco Ortega <francopdx@gmail.com>
2021-02-09 14:44:12 -0800

    add getCharacters service and test display of fetched data in class component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,40 @@
-import React from 'react';
+// import React from 'react';
 
-export default function App() {
-  return <h1>Hello World</h1>;
+// export default function App() {
+
+//   return <h1>Hello World</h1>;
+// }
+
+import React, { Component } from 'react';
+import getCharacters from '../../services/getCharacters';
+
+export default class App extends Component {
+
+  state = {
+    characters: []
+  }
+
+  componentDidMount() {
+    getCharacters()
+      .then(characters => this.setState({ characters }));
+  }
+
+
+  render() {
+    console.log(this.state.characters);
+    return (
+      <div>
+      Lord of the Rings Characters
+        <ul>
+          {this.state.characters.map(character => 
+            <li key={character.id}>
+              <p>
+              Character: {character.name}
+              </p>
+            </li>
+          )}
+        </ul>
+      </div>
+    );
+  }
 }
