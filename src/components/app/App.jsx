425fdd7b05628e5e1bcfceb35f78488495adb150
Franco Ortega <francopdx@gmail.com>
2021-02-09 14:51:35 -0800

    add initial files and folders to display a list of characters

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,40 +1,47 @@
-// import React from 'react';
+import React from 'react';
+import CharacterList from './characters/CharacterList';
 
-// export default function App() {
+export default function App() {
 
-//   return <h1>Hello World</h1>;
-// }
-
-import React, { Component } from 'react';
-import getCharacters from '../../services/getCharacters';
-
-export default class App extends Component {
-
-  state = {
-    characters: []
-  }
-
-  componentDidMount() {
-    getCharacters()
-      .then(characters => this.setState({ characters }));
-  }
-
-
-  render() {
-    console.log(this.state.characters);
-    return (
-      <div>
-      Lord of the Rings Characters
-        <ul>
-          {this.state.characters.map(character => 
-            <li key={character.id}>
-              <p>
-              Character: {character.name}
-              </p>
-            </li>
-          )}
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <>
+      <CharacterList />
+    </>
+  );
 }
+
+
+
+// import React, { Component } from 'react';
+// import getCharacters from '../../services/getCharacters';
+
+// export default class App extends Component {
+
+//   state = {
+//     characters: []
+//   }
+
+//   componentDidMount() {
+//     getCharacters()
+//       .then(characters => this.setState({ characters }));
+//   }
+
+
+//   render() {
+//     console.log(this.state.characters);
+//     return (
+//       <div>
+//       Lord of the Rings Characters
+//         <ul>
+//           {this.state.characters.map(character => 
+//             <li key={character.id}>
+//               <p>
+//               Character: {character.name}
+//               </p>
+//             </li>
+//           )}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
---
+++ src/components/app/characters/CharacterList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Character from './Character';
+
+export default function CharacterList() {
+  return (
+    <div>
+      List of Characters
+      <p><Character /></p>
+    </div>
+  );
+}
