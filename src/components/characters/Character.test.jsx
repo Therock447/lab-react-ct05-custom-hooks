c3645d13f42a024bd481e002bc60f402456d80e9
Franco Ortega <francopdx@gmail.com>
2021-02-09 19:57:37 -0800

    update Character test

--- src/components/characters/Character.test.jsx
+++ src/components/characters/Character.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import Character from './Character';
 
 describe('Character component', () => {
@@ -10,5 +10,11 @@ describe('Character component', () => {
     />);
 
     screen.getByText('Character');
+
+    const character = await screen.findByTestId('character');
+
+    return waitFor(() => {
+      expect(character).not.toBeEmptyDOMElement();
+    });
   });
 });
