87388a4968b8ec218604affcbd1b76f0fda8ef5e
Franco Ortega <francopdx@gmail.com>
2021-02-09 17:58:25 -0800

    add MemoryRouter to CharacterList test

--- src/components/characters/CharacterList.test.jsx
+++ src/components/characters/CharacterList.test.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import CharacterList from './CharacterList';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('CharacterList component', () => {
   it('displays a list of characters', async() => {
-    render(<CharacterList />);
+    render(
+      <MemoryRouter>
+        <CharacterList />
+      </MemoryRouter>
+    );
 
     screen.getByText('Loading');
 
