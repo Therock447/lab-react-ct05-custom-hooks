1c019638b3237c67d95e67cc17cebfbcb83835c6
Franco Ortega <francopdx@gmail.com>
2021-02-09 19:43:21 -0800

    attempt to add mocks to tests

--- src/components/characters/CharacterList.test.jsx
+++ src/components/characters/CharacterList.test.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import CharacterList from './CharacterList';
 import { MemoryRouter } from 'react-router-dom';
+// import { rest } from 'msw';
+// import { setUpServer } from 'msw/node';
+import CharacterList from './CharacterList';
+// import allCharacters from '../../fixtures/allCharacters.json';
+
+// const server = setUpServer(
+//   rest.get('https://the-one-api.dev/v2/character/', (req, res, ctx) => {
+//     return res(ctx.json(allCharacters));
+//   })
+// );
 
 describe('CharacterList component', () => {
+  // beforeAll(() => server.listen());
+  // afterAll(() => server.close());
   it('displays a list of characters', async() => {
     render(
       <MemoryRouter>
--- src/components/characters/OneCharacterDisplay.test.jsx
+++ src/components/characters/OneCharacterDisplay.test.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
+// import { rest } from 'msw';
+// import { setUpServer } from 'msw/node';
 import OneCharacterDisplay from './OneCharacterDisplay';
+// import oneCharacterByIdResponse from '../../fixtures/oneCharacterById.json';
+
+// const server = setUpServer(
+//   rest.get('https://the-one-api.dev/v2/character/5cd99d4bde30eff6ebccfbbe', (req, res, ctx) => {
+//     return res(ctx.json(oneCharacterByIdResponse));
+//   })
+// );
 
 describe('OneCharacterDisplay component', () => {
-  it('displays component with details of one character', async() => {
+  // beforeAll(() => server.listen());
+  // afterAll(() => server.close());
+
+  it('fetches and displays details of one character', async() => {
     render(<OneCharacterDisplay
       match={{ params: { id: '5cd99d4bde30eff6ebccfbbe' } }}
     />);
@@ -13,6 +25,9 @@ describe('OneCharacterDisplay component', () => {
     return waitFor(() => {
       screen.findByText('Adanel');
       screen.findByText('Human');
+      screen.findByText('No birth date available.');
+      screen.findByText('No death date available.');
+      screen.findByText('Bio');
     });
   });
 });
