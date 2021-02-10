c61a5fa95662d729b222652d9a8cdf138447852b
Franco Ortega <francopdx@gmail.com>
2021-02-10 13:02:23 -0800

    add styling and fetched data to Pagination; no actual pagination yet

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,15 +1,28 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useCharacters } from '../../hooks/characters';
 import Loading from '../loading/Loading';
 import OnePage from './OnePage';
+import styles from './Pagination.css';
 
 export default function Pagination() {
   const { loading, characters } = useCharacters();
-
   const [page, setPage] = useState(1);
 
   const decrement = () => setPage(currentPage => currentPage - 1);
   const increment = () => setPage(currentPage => currentPage + 1);
+
+  const characterElements = characters.map(character => (
+    <Link
+      key={character.id}
+      to={`character/${character.id}`}
+    >
+      <li>
+        <OnePage {...character} />
+      </li>
+    </Link>
+  ));
+
 
   if(loading) return <Loading />;
 
@@ -18,6 +31,13 @@ export default function Pagination() {
       <button onClick={decrement}>&lt;</button>
       {page}
       <button onClick={increment}>&gt;</button>
+
+      <div
+        data-testid="characters"
+        className={styles.Pagination}
+      >
+        {characterElements}
+      </div>
       <OnePage />
     </>
   );
