858cea2f99f34764baec95aa5a0ff285c3bc9d0a
Franco Ortega <francopdx@gmail.com>
2021-02-09 19:47:13 -0800

    correct pathing for mocks in tests

--- src/components/characters/CharacterList.test.jsx
+++ src/components/characters/CharacterList.test.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-// import { rest } from 'msw';
-// import { setUpServer } from 'msw/node';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import CharacterList from './CharacterList';
-// import allCharacters from '../../fixtures/allCharacters.json';
+import allCharacters from '../../fixtures/allCharacters.json';
 
-// const server = setUpServer(
-//   rest.get('https://the-one-api.dev/v2/character/', (req, res, ctx) => {
-//     return res(ctx.json(allCharacters));
-//   })
-// );
+const server = setupServer(
+  rest.get('https://the-one-api.dev/v2/character/', (req, res, ctx) => {
+    return res(ctx.json(allCharacters));
+  })
+);
 
 describe('CharacterList component', () => {
-  // beforeAll(() => server.listen());
-  // afterAll(() => server.close());
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
   it('displays a list of characters', async() => {
     render(
       <MemoryRouter>
--- src/components/characters/OneCharacterDisplay.test.jsx
+++ src/components/characters/OneCharacterDisplay.test.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-// import { rest } from 'msw';
-// import { setUpServer } from 'msw/node';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import OneCharacterDisplay from './OneCharacterDisplay';
-// import oneCharacterByIdResponse from '../../fixtures/oneCharacterById.json';
+import oneCharacterByIdResponse from '../../fixtures/oneCharacterById.json';
 
-// const server = setUpServer(
-//   rest.get('https://the-one-api.dev/v2/character/5cd99d4bde30eff6ebccfbbe', (req, res, ctx) => {
-//     return res(ctx.json(oneCharacterByIdResponse));
-//   })
-// );
+const server = setupServer(
+  // eslint-disable-next-line max-len
+  rest.get('https://the-one-api.dev/v2/character/5cd99d4bde30eff6ebccfbbe', (req, res, ctx) => {
+    return res(ctx.json(oneCharacterByIdResponse));
+  })
+);
 
 describe('OneCharacterDisplay component', () => {
-  // beforeAll(() => server.listen());
-  // afterAll(() => server.close());
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
 
   it('fetches and displays details of one character', async() => {
     render(<OneCharacterDisplay
